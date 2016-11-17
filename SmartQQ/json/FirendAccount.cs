51d5d296ed0878081b765faef7df5e2fc3c866f5
peter.peng <pen_zhaohui@126.com>
2016-11-17 17:49:06 +0800

    漏掉了两个文件

---
+++ SmartQQ/json/FirendAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartQQ.json
+{
+    internal class FirendAccount
+    {
+        public int retcode;
+        public Result result;
+        public class Result
+        {
+            public string account;
+            public string uin;
+            public string uiuin;
+        }
+    }
+}
---
+++ SmartQQ/model/MessageStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartQQ.model
+{
+    public enum MessageStatus
+    {
+        Initialized = 0,
+        GetMessage = 1,
+        SendMessage = 2,
+        GetAndSendMessage = 3
+    }
+}
